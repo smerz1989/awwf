9349e9d5a92fe40ed9352fcf8b0f6bcc371096fe
smerz1989 <stevennmerz@gmail.com>
2022-03-13 21:14:27 -0400

    Changing client side to use vue router

--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import $ from 'jquery'
+import router from './router'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
