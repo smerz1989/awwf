ab36ca4c10ffb915ec4a173905d5eb74512c0bca
smerz1989 <stevennmerz@gmail.com>
2022-03-16 15:02:38 -0400

    Making styling changes to App homepage

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,8 +1,29 @@
 <template>
-  <div id="nav">
+  <div id="nav" class="flex-nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
       <router-link to="/wordle">Wordle</router-link>
   </div>
   <router-view/>
 </template>
+
+<style>
+.flex-container {
+  display: flex;
+  height: 100%;
+  flex-wrap: nowrap;
+  justify-content: space-around;
+  align-items: center;
+}
+
+.flex-nav {
+    display: flex;
+    justify-content: center;
+}
+
+.flex-container > .div {
+  text-align: center;
+  height: 100%;
+  background-color: 351431;
+}
+</style>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -6,3 +6,22 @@
 
 <script>
 </script>
+
+<style>
+.form-group {
+  margin-bottom: 15px;
+}
+
+
+#app {
+  background-color: #3F3047;
+  margin: 0px;
+  min-height: 100vh;
+  padding: 0px;
+}
+html, body {
+    margin: 0px;
+    min-height: 100vh;
+    padding: 0px;
+}
+</style>
